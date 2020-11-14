a1458e41262cfbb60c1dadebf277099a0b08759d
Richard Arthur <richardarthur228@gmail.com>
2020-11-14 02:16:23 +0000

    Updated app

---
+++ src/API_KEY.js
@@ -0,0 +1,3 @@
+const API_KEY = '7339991729b7952aea80ae3f50c44810';
+
+export default API_KEY;
--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,7 @@
-const API_KEY = '7339991729b7952aea80ae3f50c44810';
+import API_KEY from "./API_KEY";
+
+// const API_KEY = API_KEY;
+
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
